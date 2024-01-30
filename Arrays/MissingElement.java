3ab9bc4955e0a3104fd0e42c7a50ece708ea0440
rakesh.t <rakesh.t@agsindia.com>
2024-01-30 16:50:28 +0530

    Missing Element Using O(N) TC and O(1) Sc Approach

---
+++ Arrays/MissingElement.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class MissingElement {
+    public static void main(String[] args) {
+        int[] arr = {9,6,4,2,3,5,7,0,1};
+        System.out.println(missingNumber(arr));
+    }
+
+    public static int missingNumber(int[] arr){
+        int n = arr.length;
+        int sum1 = (n*(n+1))/2,sum2=0;
+        for(int num : arr){
+            sum2 += num;
+        }
+        return sum1-sum2;
+    }
+}
