5bd341a9863b6d76819309d1e6d53f3626291adc
rakesh.t <rakesh.t@agsindia.com>
2024-01-30 12:52:27 +0530

    Package Added

--- Stacks/ReversePolishNotation.java
+++ Stacks/ReversePolishNotation.java
@@ -1,4 +1,4 @@
-/*You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+package Stacks;/*You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
 
 Evaluate the expression. Return an integer that represents the value of the expression.
 
