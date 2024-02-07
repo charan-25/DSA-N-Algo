54254dc80b6316d2bcbac2cef3fc6a0395c50c12
rakesh.t <rakesh.t@agsindia.com>
2024-02-07 21:02:58 +0530

    Bubble Sort Algorithm

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {8,2,4,-1,6,7,5,10,-1};
+        System.out.println(Arrays.toString(sort(arr)));
+    }
+
+    public static int[] sort(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+       // System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+}
