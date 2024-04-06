e1f126315d1885627b3d9251e1296ab3fc0326c5
charan <a.saicharan2502@gmail.com>
2024-04-06 18:37:44 +0530

    Minimum Removals To Make Valid Parenthesis

---
+++ Queues/MinimumRemovals.java
@@ -0,0 +1,60 @@
+package Queues;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+/*Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either'(' , ')', or lowercase English letter.*/
+public class MinimumRemovals {
+    public static void main(String[] args) {
+        System.out.println(minRemovalsToMakeValidParanthesis("lee(t(c)o)de)"));
+    }
+
+    public static String minRemovalsToMakeValidParanthesis(String str){
+        int n = str.length();
+        Deque<Integer> que = new ArrayDeque<>();
+        StringBuffer sb = new StringBuffer(str);
+        for(int i=0;i<n;i++){
+            if(str.charAt(i)=='(')
+                que.add(i);
+            else if(str.charAt(i)==')'){
+                if(que.isEmpty())
+                    sb.setCharAt(i,'#');
+                else
+                    que.pop();
+            }
+        }
+
+        while(que.size()>0)
+            sb.setCharAt(que.remove(),'#');
+        return sb.toString().replaceAll("#","");
+    }
+}
