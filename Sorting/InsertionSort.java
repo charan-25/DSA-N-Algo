74194a9209fde99cd40a9bfe3069635b966b2d71
charan <a.saicharan2502@gmail.com>
2024-04-19 23:53:21 +0530

    Minimum Increments To Make Array Unique

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -9,6 +9,7 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {10,3,6,8,2,5};
         sort(arr);
+        insertSort(arr);
     }
 
     public static void sort(int[] arr){
@@ -19,6 +20,20 @@ public class InsertionSort {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertSort(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i;j>=0;j--){
+                if(arr[j+1]<arr[j]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
