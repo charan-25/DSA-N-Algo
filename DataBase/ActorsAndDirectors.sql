7a3401b4cb92b65d9aca900d4e4f8968ac45fb72
charan <a.saicharan2502@gmail.com>
2024-05-24 19:53:56 +0530

    Queries Swapping Salary,ActorAndDirector

---
+++ DataBase/ActorsAndDirectors.sql
@@ -0,0 +1,46 @@
+/*Table: ActorDirector
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp is the primary key (column with unique values) for this table.
+
+
+Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+ActorDirector table:
++-------------+-------------+-------------+
+| actor_id    | director_id | timestamp   |
++-------------+-------------+-------------+
+| 1           | 1           | 0           |
+| 1           | 1           | 1           |
+| 1           | 1           | 2           |
+| 1           | 2           | 3           |
+| 1           | 2           | 4           |
+| 2           | 1           | 5           |
+| 2           | 1           | 6           |
++-------------+-------------+-------------+
+Output:
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
+*/
+
+SELECT actor_id,director_id
+FROM ActorDirector
+GROUP BY actor_id,director_id
+HAVING COUNT(*)>=3;
