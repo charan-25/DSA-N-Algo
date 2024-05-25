59e8c1812fac6782105f1ff463e9a15c0b7e4f75
charan <a.saicharan2502@gmail.com>
2024-05-25 23:05:15 +0530

    Creational Design Patterns

--- Streams/MapNFlatMap.java
+++ Streams/MapNFlatMap.java
@@ -5,15 +5,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class MapNFlatMap {
-    String name;
-    MapNFlatMap(){
-        this.name = "Default Name";
-        System.out.println(name);
-    }
-
-    MapNFlatMap(String name){
-        this.name = name;
-    }
     public static void main(String[] args) {
         List<String> list = List.of("abcdef",
                 "ab",
@@ -24,9 +15,9 @@ public class MapNFlatMap {
         List<Integer> res = list.stream().map(String::length).collect(Collectors.toList());
         System.out.println(res);
 
-        List<List<Integer>> flatmap = List.of(List.of(4,6,8),List.of(3,5,7),List.of(2,3,5));
-        List<Integer> distinct = flatmap.stream().flatMap(num->num.stream()).distinct().collect(Collectors.toList());
+        List<List<Integer>> flat = List.of(List.of(4,6,8),List.of(3,5,7),List.of(2,3,5));
+        List<Integer> distinct = flat.stream().flatMap(num->num.stream()).distinct().collect(Collectors.toList());
         System.out.println(distinct);
-        MapNFlatMap map = new MapNFlatMap();
+
     }
 }
